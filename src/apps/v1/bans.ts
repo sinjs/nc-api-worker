f0164d6ad1a74cc4520165b479c0367b0e48c188
sinjs <50749166+sinjs@users.noreply.github.com>
2025-01-26 01:35:23 +0100

    Reapply "redirect to new api"
    This reverts commit 763ac32c27c8a7439ae2b080972a2a33ef25f1a0.

--- src/apps/v1/bans.ts
+++ src/apps/v1/bans.ts
@@ -9,45 +9,23 @@ import { z } from 'zod';
 const app = new Hono<HonoEnv>();
 
 app.get('/', auth, admin, async (c) => {
-	const { results: bans } = await c.env.DB.prepare('SELECT * FROM ban').all<Ban>();
-	return c.json(bans);
+	return c.json({ error: 'Gone', message: 'This version has been set to read-only mode. Please use the `/v2` endpoints instead.' }, 410);
 });
 
 app.get('/:userId', async (c) => {
 	const userId = c.req.param('userId');
-	const ban = await c.env.DB.prepare('SELECT * FROM ban WHERE userId = ?').bind(userId).first<Ban>();
+
+	const ban = await fetch(`https://api.nigga.church/v2/bans/${userId}`).then((response) => response.json());
 
 	if (!ban) return c.json({ banned: false });
 	return c.json({ banned: true, ban });
 });
 
-app.put('/', auth, admin, async (c) => {
-	const schema = z.object({
-		userId: z.string(),
-		reason: z
-			.string()
-			.optional()
-			.transform((a) => a ?? null),
-		expires: z.coerce
-			.date()
-			.optional()
-			.transform((a) => a ?? null),
-	});
-
-	const body = schema.parse(await c.req.json());
-
-	await c.env.DB.prepare('INSERT INTO ban (userId, reason, expires) VALUES (?1, ?2, ?3)')
-		.bind(body.userId, body.reason, body.expires)
-		.run();
-
-	return c.body(null, 201);
-});
-
-app.delete('/:userId', auth, admin, async (c) => {
-	const userId = c.req.param('userId');
-	await c.env.DB.prepare('DELETE FROM ban WHERE userId = ?').bind(userId).run();
-
-	return c.body(null, 204);
-});
+app.put('/', (c) =>
+	c.json({ error: 'Gone', message: 'This version has been set to read-only mode. Please use the `/v2` endpoints instead.' }, 410),
+);
+app.delete('/:userId', (c) =>
+	c.json({ error: 'Gone', message: 'This version has been set to read-only mode. Please use the `/v2` endpoints instead.' }, 410),
+);
 
 export default app;
